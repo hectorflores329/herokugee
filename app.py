0a1be5981bffbed12ff70ee2cbcfe7832f13803a
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-19 22:21:20 -0600

    Commit

--- app.py
+++ app.py
@@ -1,72 +1,35 @@
 from flask import Flask
 import folium
-import folium.plugins as plugins
-import numpy as np
-import pandas as pd
-from datetime import datetime, timedelta
-from folium.plugins import FloatImage
-from folium.plugins import Draw
-from folium.plugins import MiniMap
-import folium
-from folium import plugins
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def mapa():
 
-    # Add custom basemaps to folium
-    basemaps = {
-        'Google Maps': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Maps',
-            overlay = True,
-            control = True
-        ),
-        'Google Satellite': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
-        ),
-        'Google Terrain': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Terrain',
-            overlay = True,
-            control = True
-        ),
-        'Google Satellite Hybrid': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
-        ),
-        'Esri Satellite': folium.TileLayer(
-            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            attr = 'Esri',
-            name = 'Esri Satellite',
-            overlay = True,
-            control = True
+    m = folium.Map(
+        location=[-33.48621795345005, -70.66557950912359],
+        zoom_start=5,
+        min_zoom = 8,
+        max_zoom = 30,
+        control_scale=True
+        # tiles = "openstreetmap"
         )
-    }
 
-    
-    # Set visualization parameters.
-    vis_params = {
-    'min': 0,
-    'max': 4000,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+    w = folium.WmsTileLayer(url = 'https://ide.dataintelligence-group.com/geoserver/chile/wms',
+                        layers = 'chile:Regiones',
+                        fmt ='image/png',
+                        transparent = True,
+                        name = "Regiones",
+                        control = True,
+                        attr = "Mapa de Chile",
+                        overlay=True,
+                        control=True,
+                        show=True
+                        )
 
-    # Create a folium map object.
-    m = folium.Map(location=[40.33, -99.42], zoom_start=4)
+    w.add_to(m)
 
-    # Add custom basemaps
-    basemaps['Google Maps'].add_to(m)
-    basemaps['Google Satellite Hybrid'].add_to(m)
 
     
     return m._repr_html_()
