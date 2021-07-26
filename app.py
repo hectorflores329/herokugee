deed21eab29ce0fc236ddc3c8ff303b24d2f5bcf
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 10:49:57 -0600

    Commit

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 def mapa():
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
+    Map = geemap.Map(center=[40,-100], zoom=4).add_to(m)
+
     return m._repr_html_()
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
