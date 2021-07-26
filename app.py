ea22b3fa1d1a92da4f0c7647d01fab272294342f
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 13:19:10 -0600

    Commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def mapa():
     dem = ee.Image('USGS/SRTMGL1_003')
     xy = ee.Geometry.Point([86.9250, 27.9881])
     elev = dem.sample(xy, 30).first().get('elevation').getInfo()
-    print('Mount Everest elevation (m):', elev)
+    # print('Mount Everest elevation (m):', elev)
 
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
