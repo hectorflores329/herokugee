f678ad75f0c8113a7302bd1b50a2c0a0d55333fb
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 11:33:14 -0600

    Commit

--- app.py
+++ app.py
@@ -8,13 +8,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def mapa():
+
+    # Get a composite of all Sentinal 2 images within a date range that include my point of interest.
+    poi = ee.Geometry.Point([-82.4572, 27.9506])
+    image = ee.ImageCollection('COPERNICUS/S2').filterBounds(poi).filterDate('2016-10-01', '2016-12-31').min()
     
-    ee.Initialize()
-    image = ee.Image('USGS/SRTMGL1_003')
+    Map = folium.Map(location=[27.9506, -82.4572], zoom_start=8)# To see a google satellite view as a basemap
+    Map.setOptions('HYBRID')
+    
+    nir = image.select('B8')
+    red = image.select('B4')
 
-    m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
+    ndvi = nir.subtract(red).divide(nir.add(red)).rename('NDVI Layer')
+    ndviParams = {'min': -1, 'max': 1, 'palette': ['blue', 'white', 'green']}
+    
+    Map.addLayer(ndvi, ndviParams, 'NDVI image')
+    Map.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
 
-    return m._repr_html_()
+    return Map._repr_html_()
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
 if __name__ == '__main__':
