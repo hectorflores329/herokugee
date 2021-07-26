2b5177870d38351639edcdac6a0178f625f9ce48
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 12:26:45 -0600

    Commit

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import geemap
 
 from flask import Flask
 import folium
+from folium import plugins
 
 app = Flask(__name__)
 
 @app.route('/')
 def mapa():
+
+    ee.Authenticate()
+    ee.Initialize()
     
+    # Print the elevation of Mount Everest.
+    dem = ee.Image('USGS/SRTMGL1_003')
+    xy = ee.Geometry.Point([86.9250, 27.9881])
+    elev = dem.sample(xy, 30).first().get('elevation').getInfo()
+    print('Mount Everest elevation (m):', elev)
+
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
     return m._repr_html_()
