1789b9574068b2b4f39460817119e479b4e24368
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 16:32:51 -0600

    Commit

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ def mapa():
 
     # Map = geemap.Map(center=[-33.48621795345005, -70.66557950912359], zoom=4)
     
-    Map = geemap.Map(center=[-33.48621795345005, -70.66557950912359], zoom=4)
+    Map = geemap.Map()
     
-    m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
+    # m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
-    return m._repr_html_()
+    # return m._repr_html_()
+    return "Hola mundo"
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
 if __name__ == '__main__':
