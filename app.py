b272788082c5488116aac188f9bd9a7bad39e2ca
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 13:12:39 -0600

    Commit

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 @app.route('/')
 def mapa():
 
-    '''ee.Authenticate()
+    # ee.Authenticate()
     ee.Initialize()
     
     # Print the elevation of Mount Everest.
     dem = ee.Image('USGS/SRTMGL1_003')
     xy = ee.Geometry.Point([86.9250, 27.9881])
     elev = dem.sample(xy, 30).first().get('elevation').getInfo()
-    print('Mount Everest elevation (m):', elev)'''
+    print('Mount Everest elevation (m):', elev)
 
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
