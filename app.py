7c9e13422302027a34f5b6d4e3fac81b4966a738
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-26 11:59:32 -0600

    Commit

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def mapa():
     
-    def add_ee_layer(self, ee_image_object, vis_params, name):
-        map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-        folium.raster_layers.TileLayer(
-            tiles=map_id_dict['tile_fetcher'].url_format,
-            attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
-            name=name,
-            overlay=True,
-            control=True
-        ).add_to(self)
-
-    folium.Map.add_ee_layer = add_ee_layer
-
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
     return m._repr_html_()
