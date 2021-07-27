cd931f084b138522eb4572118e807d266edc89c6
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-27 16:29:23 -0600

    Commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def mapa():
     
     # Get a composite of all Sentinal 2 images within a date range that include my point of interest.
-    poi = ee.Geometry.Point([-82.4572, 27.9506])
+    point = ee.Geometry.Point([-82.4572, 27.9506])
 
     m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
     return m._repr_html_()
