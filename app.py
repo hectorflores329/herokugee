2265ba0fae72fcf13256dd155adeaf116ff85be9
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-27 13:49:41 -0600

    Commit

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from flask import Flask
 import folium
 from folium import plugins
 
+import os
+
+from .common import *
+from .conversion import *
+from .legends import builtin_legends
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,12 +20,12 @@ def mapa():
 
     # Map = geemap.Map(center=[-33.48621795345005, -70.66557950912359], zoom=4)
     
-    Map = geemap.Map()
+    # Map = geemap.Map()
     
-    # m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
+    m = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=4)
 
-    # return m._repr_html_()
-    return "Hola mundo"
+    return m._repr_html_()
+    # return "Hola mundo"
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
 if __name__ == '__main__':
