295e10d410a4210cb26338ad36324ca3f2416bd7
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-27 19:05:06 -0600

    Commit

--- app.py
+++ app.py
@@ -12,38 +12,38 @@ def mapa():
     # folium.Marker([-33.48621795345005, -70.66557950912359], popup='Plaza Mayor').add_to(m)
 
     html = "<p>Latitud: 40.0</p><p>Longitud: 2.1</p>"
-    iframe1 = branca.element.IFrame(html=html, width=500, height=300)
+    iframe1 = branca.element.IFrame(html=html, width=250, height=200)
     html = "<p>Latitud: 40.0</p><p>Longitud: 3.5</p>"
-    iframe2 = branca.element.IFrame(html=html, width=500, height=300)
+    iframe2 = branca.element.IFrame(html=html, width=250, height=200)
     html = "<p>Latitud: 39.0</p><p>Longitud: 2.1</p>"
-    iframe3 = branca.element.IFrame(html=html, width=500, height=300)
+    iframe3 = branca.element.IFrame(html=html, width=250, height=200)
     html = "<p>Latitud: 39.0</p><p>Longitud: 3.5</p>"
-    iframe4 = branca.element.IFrame(html=html, width=500, height=300)
+    iframe4 = branca.element.IFrame(html=html, width=250, height=200)
 
     marcador1 = folium.Marker(
-        location=(40, 2.1),
+        location=(-33.2993248483003, -70.84960050794211),
         popup=folium.Popup(iframe1, max_width=500),
         icon=folium.Icon(color="black")
     )
     marcador2 = folium.Marker(
-        location=(40, 3.5),
+        location=(-33.62240730758864, -70.84960050794211),
         popup=folium.Popup(iframe2, max_width=500),
         icon=folium.Icon(color="gray")
     )
     marcador3 = folium.Marker(
-        location=(39, 2.1),
+        location=(-33.560633705782394, -70.43761319983437),
         popup=folium.Popup(iframe3, max_width=500),
         icon=folium.Icon(color="black")
     )
     marcador4 = folium.Marker(
-        location=(39, 3.5),
+        location=(-33.2844019692712, -70.45821256523976),
         popup=folium.Popup(iframe4, max_width=500),
         icon=folium.Icon(color="gray")
     )
 
     # Creamos dos grupos para los marcadores
-    grp_este = folium.FeatureGroup(name='Este')
-    grp_oeste = folium.FeatureGroup(name='Oeste')
+    grp_este = folium.FeatureGroup(name='Grupo 1')
+    grp_oeste = folium.FeatureGroup(name='Grupo 2')
     # Añadimos los marcadores AL GRUPO AL QUE CORRESPONDAN (NO AL MAPA)
     marcador1.add_to(grp_oeste)
     marcador2.add_to(grp_este)
