2fa603d8c2ca6fbb7cbd3485d7436486dec11fac
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-02 23:52:25 -0600

    Commit

--- app.py
+++ app.py
@@ -22,12 +22,16 @@ def mapa():
     )
 
     plugins.TimestampedGeoJson(
-    {
-        "type": "FeatureCollection",
-        "features": mediambiente,
-    },
-        period="PT1M",
+        {"type": "FeatureCollection", "features": mediambiente},
+        period="P1M",
         add_last_point=True,
+        auto_play=False,
+        loop=False,
+        max_speed=1,
+        loop_button=True,
+        date_options="YYYY/MM/DD",
+        time_slider_drag_update=True,
+        duration="P2M",
     ).add_to(m)
 
     def getcolor(feature):
