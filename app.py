30d59a9ed494e7d5aa44618e161f5260858f1a33
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-03 00:50:19 -0600

    Commit

--- app.py
+++ app.py
@@ -3,21 +3,20 @@ from flask import Flask
 import branca.colormap as cm
 from branca.element import Template, MacroElement
 import folium.plugins as plugins
-import urllib.request, json 
 
 app = Flask(__name__)
 
 @app.route('/')
 def mapa():
     
-    def downloadJson(link):
+    '''def downloadJson(link):
         
         with urllib.request.urlopen(link) as url:
             data = json.loads(url.read().decode())
         
-        return data
+        return data'''
 
-    spatial_temporal_data = downloadJson("https://raw.githubusercontent.com/ghandic/folium/master/examples/data/house_movement.json")
+    # spatial_temporal_data = downloadJson("https://raw.githubusercontent.com/ghandic/folium/master/examples/data/house_movement.json")
 
     m = folium.Map(
         location=[56.096555, -3.64746],
@@ -25,14 +24,14 @@ def mapa():
         zoom_start=5
     )
 
-    plugins.TimestampedGeoJson(spatial_temporal_data, 
+    '''plugins.TimestampedGeoJson(spatial_temporal_data, 
                 period='P1M', add_last_point=True,
                 auto_play=False, loop=False, 
                 maxSpeed=1, loopButton=True,
                 dateOptions='YYYY/MM/DD',
                 timeSliderDragUpdate=True).add_to(m)
 
-    return m._repr_html_()
+    return m._repr_html_()'''
 
 
 if __name__ == '__main__':
