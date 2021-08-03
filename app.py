8d2d34166172ae73b3bf2ff4861af0ca4500bb26
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-03 01:12:11 -0600

    Commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def mapa():
         
         return data
 
-    # spatial_temporal_data = downloadJson("https://raw.githubusercontent.com/ghandic/folium/master/examples/data/house_movement.json")
+    spatial_temporal_data = downloadJson("https://raw.githubusercontent.com/ghandic/folium/master/examples/data/house_movement.json")
 
     m = folium.Map(
         location=[56.096555, -3.64746],
@@ -25,12 +25,12 @@ def mapa():
         zoom_start=5
     )
 
-    '''plugins.TimestampedGeoJson(spatial_temporal_data, 
-                period='P1M', add_last_point=True,
-                auto_play=False, loop=False, 
-                maxSpeed=1, loopButton=True,
-                dateOptions='YYYY/MM/DD',
-                timeSliderDragUpdate=True).add_to(m)'''
+    plugins.TimestampedGeoJson(spatial_temporal_data, 
+                period='P1M', 
+                add_last_point=True,
+                auto_play=False, 
+                loop=True, 
+                ).add_to(m)
 
     return m._repr_html_()
 
