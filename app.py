b8195db49a480ba36d44b778905356ff32e97335
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-05 00:29:50 -0600

    Commit

--- app.py
+++ app.py
@@ -1,65 +1,23 @@
-import folium
 from flask import Flask
-import json
-import requests
+import pandas as pd
+import folium
 
 app = Flask(__name__)
 
 @app.route('/')
-def mapa():
+def temp():
+
     
-    url = (
-        "https://raw.githubusercontent.com/hectorflores329/herokugee/main"
-    )
-    vis1 = json.loads(requests.get(f"{url}/vis1.json").text)
-    vis2 = json.loads(requests.get(f"{url}/vis2.json").text)
-    vis3 = json.loads(requests.get(f"{url}/vis3.json").text)
-    vis4 = json.loads(requests.get(f"{url}/vis4.json").text)
+    _map = folium.Map(
+        location=[40.712776, -74.005974],
+        zoom_start=5,
+        width = 850,
+        height = 650,
+        min_zoom = 8,
+        max_zoom = 14
+        )
 
-    m = folium.Map(
-        location=[-33.48621795345005, -70.66557950912359],
-        zoom_start=8,
-        control_scale=True,
-    )
-
-    folium.CircleMarker(
-        location=[-32.41681831859102, -70.57579231998415],
-        fill=True,
-        radius=8,
-        popup=folium.Popup(max_width=450).add_child(
-            folium.Vega(vis1, width=450, height=250)
-        ),
-    ).add_to(m)
-
-    folium.CircleMarker(
-        location=[-35.346523319705604, -71.34401806293496],
-        fill=True,
-        radius=8,
-        popup=folium.Popup(max_width=450).add_child(
-            folium.Vega(vis2, width=450, height=250)
-        ),
-    ).add_to(m)
-
-    folium.CircleMarker(
-        location=[-37.3530323621873, -72.25758381593647],
-        fill=True,
-        radius=8,
-        popup=folium.Popup(max_width=450).add_child(
-            folium.Vega(vis3, width=450, height=250)
-        ),
-    ).add_to(m)
-
-    folium.CircleMarker(
-        location=[-33.467890412071654, -70.66557950912359],
-        fill=True,
-        radius=8,
-        popup=folium.Popup(max_width=450).add_child(
-            folium.Vega(vis4, width=450, height=250)
-        ),
-    ).add_to(m)
-
-    return m._repr_html_()
-
+    return _map._repr_html_()
 
 if __name__ == '__main__':
     app.run()
