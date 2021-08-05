6eb42bbc54aaf80949d57ac63f61e27a88372508
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-05 00:38:49 -0600

    Commit

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def temp():
 
+    url = (
+        "https://raw.githubusercontent.com/hectorflores329/herokugee/main"
+    )
+    puntos = f"{url}/Región Metropolitana de Santiago, TEMP.xlsx"
+    df = pd.read_excel(puntos)
+
     _map = folium.Map(
         location=[-33.467890412071654, -70.66557950912359],
         zoom_start=4,
