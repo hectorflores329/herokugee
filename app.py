acc8e706e5022f8874980278166cbe37205b15fd
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-06 14:20:54 -0600

    Commit

--- app.py
+++ app.py
@@ -40,21 +40,20 @@ def temp():
     
     for coord in locations:
         html="""
-        <div class="chart-wrap horizontal">
-        <div class="title">TEMPERATURA: """ + coord[2] + """</div>
+        <div>
+        <div>TEMPERATURA: """ + coord[2] + """</div>
             <br>
-            <div class="grid">
+            <div>
 
-                <div class="bar" style="--bar-value:85%;" data-name="Your Blog" title="Your Blog 85%"></div>
-                <div class="bar" style="--bar-value:23%;" data-name="Medium" title="Medium 23%"></div>
-                <div class="bar" style="--bar-value:7%;" data-name="Tumblr" title="Tumblr 7%"></div>
-                <div class="bar" style="--bar-value:38%;" data-name="Facebook" title="Facebook 38%"></div>
-                <div class="bar" style="--bar-value:35%;" data-name="YouTube" title="YouTube 35%"></div>
-                <div class="bar" style="--bar-value:30%;" data-name="LinkedIn" title="LinkedIn 30%"></div>
-                <div class="bar" style="--bar-value:5%;" data-name="Twitter" title="Twitter 5%"></div>
-                <div class="bar" style="--bar-value:20%;" data-name="Other" title="Other 20%"></div>
-                
-
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            
             </div>
         </div>
         """
