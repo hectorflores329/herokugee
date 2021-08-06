12cdad34d4744057287f579c8a0ef58b08192bca
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-06 02:52:06 -0600

    Commit

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ def temp():
     iframe = folium.IFrame(html=html, width=200, height=100)
 
     for coord in locations:
-        folium.CircleMarker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=8, popup=coord[2][0]).add_to(_map)
+        # folium.CircleMarker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=8, popup=coord[2][0]).add_to(_map)
+        folium.CircleMarker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=8, popup=folium.Popup(iframe)).add_to(_map)
 
     return _map._repr_html_()
 
