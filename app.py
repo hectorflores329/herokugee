88fa2dca86d53e348f118e95829ca37ead4ac71e
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-05 23:38:51 -0600

    Commit

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def temp():
         )
     
     folium.CircleMarker(
-        location=[-32.41681831859102, -70.57579231998415],
+        location=([-32.41681831859102, -70.57579231998415], [-33.41681831859102, -70.57579231998415]),
         fill=True,
         radius=8,
-        popup="Hola",
+        popup="TE AMO ♥",
     ).add_to(_map)
 
     return _map._repr_html_()
