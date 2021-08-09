50c368ec8f30aa0642eedb4c68a96a6e7a729690
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-09 14:08:13 -0600

    Commit

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def temp():
         # folium.CircleMarker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=8, popup=coord[2][0]).add_to(_map)
         # folium.CircleMarker(location=[df["latitude"][i],df["longitude"][i]], fill_color=df["Simbología"][i], radius=8, tooltip=df["NOM_COMUNA"][i], popup=folium.Popup(iframe)).add_to(_map)
         
-        _map.add_child(FastMarkerCluster(df["latitude"][i],df["longitude"].values.tolist()))
+        _map.add_child(FastMarkerCluster(df["latitude"], df["longitude"].values.tolist()))
 
         # FastMarkerCluster(data=list(zip(df["latitude"][i],df["longitude"][i]))).add_to(_map)
         folium.LayerControl().add_to(_map)
