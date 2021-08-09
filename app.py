728ce715072a98e7043a38c26a0edaf5f3f17c1c
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-09 14:11:42 -0600

    Commit

--- app.py
+++ app.py
@@ -108,14 +108,20 @@ def temp():
         </div>
 
         """
-        # iframe = folium.IFrame(html=html, width=350, height=350)
-
-        # folium.CircleMarker(location=[coord[0], coord[1]], fill_color='#43d9de', radius=8, popup=coord[2][0]).add_to(_map)
+        iframe = folium.IFrame(html=html, width=350, height=350)
         # folium.CircleMarker(location=[df["latitude"][i],df["longitude"][i]], fill_color=df["Simbología"][i], radius=8, tooltip=df["NOM_COMUNA"][i], popup=folium.Popup(iframe)).add_to(_map)
         
-        _map.add_child(FastMarkerCluster(df["latitude"], df["longitude"].values.tolist()))
+        popup = folium.Popup(iframe, max_width=2650)
+        
+        folium.Marker(
+            location=[[df["latitude"][i],df["longitude"][i]]],
+            popup=popup,
+            icon=folium.DivIcon(html=f"""
+                <div><svg>
+                    <circle cx="35" cy="35" r="20" fill="#69b3a2" opacity="1"/>
+                </svg></div>""")
+        ).add_to(_map)
 
-        # FastMarkerCluster(data=list(zip(df["latitude"][i],df["longitude"][i]))).add_to(_map)
         folium.LayerControl().add_to(_map)
 
     return _map._repr_html_()
