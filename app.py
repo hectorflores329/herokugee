7ca27a55237dfd154edae523ece39279d05c52b4
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-10 17:26:03 -0600

    Commit

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ def temp():
         locations.append((lat, lon, nomCom))
 
     if (periodo == "Simbologia2001"):
-        ubicacion = [-33.3590800, -70.5086900]
+        ubicacion = [-33.3090800, -70.5086900]
     else:
-        ubicacion = [-33.3590800, -70.5086900]
+        ubicacion = [-33.3090800, -70.5086900]
     
     _map = folium.Map(
         location=ubicacion,
